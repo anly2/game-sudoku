9e405d516c5ea8718799649813beb2ec7f8122ca
Anko Anchev <anko.anchev@gmail.com>
2018-11-18 13:15:01 +0000

    added placeholder personal solver

---
+++ sudoku-solver/src/main/java/me/aanchev/sudoku/solvers/impl/humanoid/aa/AASudokuSolver.java
@@ -0,0 +1,15 @@
+package me.aanchev.sudoku.solvers.impl.humanoid.aa;
+
+import me.aanchev.sudoku.model.SudokuGrid;
+import me.aanchev.sudoku.solvers.SudokuMove;
+import me.aanchev.sudoku.solvers.SudokuSolver;
+
+import java.util.stream.Stream;
+
+public class AASudokuSolver implements SudokuSolver {
+
+    @Override
+    public Stream<SudokuMove> play(SudokuGrid grid) {
+        return null;
+    }
+}
